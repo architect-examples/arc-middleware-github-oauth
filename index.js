30f5aa83fb24723936b0427fb0f8eb1c87446c5e
Brian LeRoux <b@brian.io>
2017-11-18 17:50:36 -0800

    hbd

---
+++ index.js
@@ -0,0 +1,52 @@
+var waterfall = require('run-waterfall')
+var authGithub = require('./00-auth-github-account')
+var getGithub = require('./01-get-github-account')
+
+module.exports = function auth(req, res, next) {
+
+  if (req.session.isLoggedIn) {
+    res({
+      location: req._url(`/`)
+    })
+  }
+  else if (req.query.code) {
+
+    var clientID = process.env.GITHUB_CLIENT_ID
+    var clientSecret = process.env.GITHUB_CLIENT_SECRET
+    var code = req.query.code
+
+    waterfall([
+      function _auth(callback) {
+        authGithub({
+          clientID, 
+          clientSecret, 
+          code
+        }, callback) 
+      },
+      function _account(token, callback) {
+        getGithub({
+          token
+        }, callback)
+      }
+    ],
+    function _done(err, account) {
+      if (err && err.code === 403) {
+        res({
+          location: req._url(`/`)
+        })
+      }
+      else if (err) {
+        res(err)
+      }
+      else {
+        res({
+          session: account,
+          location: req._url(`/`)
+        })
+      }
+    })
+  }
+  else {
+    next()
+  }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+## arc-middleware-github-oauth
+
+Expects the following environment variables to be configured:
+
+- `GITHUB_USERNAME`
+- `GITHUB_CLIENT_ID`
+- `GITHUB_CLIENT_SECRET`
